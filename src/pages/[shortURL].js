4254d3910512bb3088454933c1592da67e7227ad
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-10-01 18:18:02 +0200

    Refactor: Update API endpoint in [shortURL].js

--- src/pages/[shortURL].js
+++ src/pages/[shortURL].js
@@ -1,25 +1,38 @@
 async function getLinkUrl(shortURL) {
   try {
     const response = await fetch(
-      `http://localhost:8080/api/shorturl/${shortURL}`
+      `http://localhost:8080/api/redirect/${shortURL}`
     );
-    const data = await response.json();
-    return data.url;
+
+    if (!response.ok) {
+      throw new Error('Error fetching URL');
+    }
+
+    return response.url;
   } catch (error) {
-    console.error(error);
+    console.error('Fetch error:', error);
     return null;
   }
 }
 
-export async function GET({ params, redirect }) {
+export async function GET({ params }) {
   const { shortURL } = params;
 
   const link = await getLinkUrl(shortURL);
 
   if (!link) {
-    console.log('No link found');
-    return redirect('/expired', 301);
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: '/expired',
+      },
+    });
   }
 
-  return redirect(link, 301);
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: link, // Redirige a la URL final
+    },
+  });
 }
